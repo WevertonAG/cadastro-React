b6c67104a2179bccba2203b101d2b5c9f5c693d4
Weverton Guimarães <weverton552@gmail.com>
2022-01-03 18:15:30 -0300

    c

---
+++ Pasta/frontend/src/main/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+import React from 'react'
+
+import Logo from '../components/templates/Logo'
+import Nav from '../components/templates/Nav'
+import Main from '../components/templates/Main'
+import Footer from '../components/templates/Footer'
+
+export default props =>
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Main icon="home" title="Início"
+            subtitle="Segundo Projeto do Capitulo de React."/>
+        <Footer />
+    </div>
